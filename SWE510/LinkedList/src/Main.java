e1c35ccfe5f8ba1dc8a85f2dc7c88f8db0af44ae
Mehmet Eyüpoğlu <59320966+mehmeteyupoglu@users.noreply.github.com>
2023-03-06 21:44:26 +0300

    add push functionality to llist

--- SWE510/LinkedList/src/Main.java
+++ SWE510/LinkedList/src/Main.java
@@ -1,12 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
         MyLinkedList list = new MyLinkedList();
     }
 }
 
 class MyLinkedList {
     Node head;
+
+    public void push(int x){
+        Node new_node = new Node(x);
+        head = new_node;
+        new_node.next = head;
+    }
 }
 
 class Node {
