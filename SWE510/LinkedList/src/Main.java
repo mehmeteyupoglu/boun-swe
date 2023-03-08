d4f9efac807905cfaac4c17fd165c44c07a761fc
Mehmet Eyüpoğlu <59320966+mehmeteyupoglu@users.noreply.github.com>
2023-03-08 12:24:06 +0300

    update linked list

--- SWE510/LinkedList/src/Main.java
+++ SWE510/LinkedList/src/Main.java
@@ -1,6 +1,25 @@
 public class Main {
     public static void main(String[] args) {
         MyLinkedList list = new MyLinkedList();
+        System.out.println(list.head);
+        for (int i = 0; i < 4; i++) {
+            list.append(i);
+        }
+
+        renderLinkedList(list);
+    }
+
+    public static void renderLinkedList(MyLinkedList list){
+        Node current = list.head;
+
+        while(current.next != null){
+            System.out.println(current.data);
+            current = current.next;
+        }
+
+        System.out.println(current.data);
+
+        System.out.println("list rendered...");
     }
 }
 
@@ -9,8 +28,27 @@ class MyLinkedList {
 
     public void push(int x){
         Node new_node = new Node(x);
-        head = new_node;
         new_node.next = head;
+        head = new_node;
+    }
+
+    public void append(int x){
+        Node current = head;
+        Node new_node = new Node(x);
+//        System.out.println(x);
+
+        if(head == null){
+            head = new_node;
+            return;
+        }
+
+        new_node.next = null;
+
+        while((current.next != null)){
+            current = current.next;
+        }
+
+        current.next = new_node;
     }
 }
 
