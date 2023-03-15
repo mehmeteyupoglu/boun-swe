760a7bbd6f6923cbfd9eaf634538ae5fcac4c926
Mehmet Eyüpoğlu <59320966+mehmeteyupoglu@users.noreply.github.com>
2023-03-15 09:52:05 +0300

    feat: add stack

--- SWE510/Stacks/src/Main.java
+++ SWE510/Stacks/src/Main.java
@@ -1,5 +1,26 @@
+import java.util.*;
+import java.io.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Stack myStack = new Stack();
+
+        Stack<String> myStringStack = new Stack<String>();
+
+        myStack.push(1);
+        myStack.push(2);
+        myStack.push(3);
+
+        myStringStack.push("A");
+        myStringStack.push("B");
+        myStringStack.push("C");
+
+        System.out.println(myStack);
+        System.out.println(myStringStack);
+
+        System.out.println("Popped element: " + myStack.pop());
+        System.out.println("Popped element: " + myStack.pop());
+
+        System.out.println("myStack after pop operations: " + myStack);
     }
 }
