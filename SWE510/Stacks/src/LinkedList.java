af850f680f872c8e49adf19317dd4b7c71ce4fb9
Mehmet Eyüpoğlu <59320966+mehmeteyupoglu@users.noreply.github.com>
2023-03-20 14:29:54 +0300

    add ll implementation of stack

---
+++ SWE510/Stacks/src/LinkedList.java
@@ -0,0 +1,42 @@
+public class LinkedList {
+    Node head = null;
+
+    public void renderLl(){
+        Node current = head;
+
+        while(current != null){
+            System.out.println(current.data);
+            current = current.next;
+        }
+    }
+
+    public void push(int x){
+        Node current = head;
+        Node new_node = new Node(x);
+
+        if(head == null){
+            head = new_node;
+            return;
+        }
+
+        while(current.next != null){
+            current = current.next;
+        }
+
+        current.next = new_node;
+    }
+
+    public int pop(){
+        if(head == null) throw new RuntimeException("Underflow");
+
+        Node current = head, prev = null;
+
+        while(current.next != null){
+            prev = current;
+            current = current.next;
+        }
+
+        prev.next = null;
+        return current.data;
+    }
+}
---
+++ SWE510/Stacks/src/LlistImplementation.java
@@ -0,0 +1,13 @@
+public class LlistImplementation {
+    public static void main(String[] args) {
+        LinkedList myLinkedList = new LinkedList();
+
+        for (int i = 0; i < 8; i++)
+            myLinkedList.push(i);
+
+        System.out.println(myLinkedList.pop());
+        System.out.println(myLinkedList.pop());
+
+        myLinkedList.renderLl();
+    }
+}
